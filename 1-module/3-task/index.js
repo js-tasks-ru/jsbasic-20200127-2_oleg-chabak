05e1602690a6d5be9f9dc708cafae1b7e96c5663
Oleg Chabak <chabak.o@brinex.ru>
2020-01-31 15:06:37 +0300

    Создал функцию приводящую к верхнему регистру первый символ

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,12 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  // Проверяем что не false, nan, undefined, '', через двойную логическую инверсию
+  if (!!str) {
+    // Первый символ приводим к верхнему регистру и конкатенируем с подстрокой из str, начиная с 1
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  return '';
 }
+
+console.log(ucFirst('t'))

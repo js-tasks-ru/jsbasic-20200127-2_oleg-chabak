2e79a82378675badfbdeff3db98a818090fc7f57
Oleg Chabak <chabak.o@brinex.ru>
2020-02-07 16:27:24 +0300

    Коммент к 5 задаче 3 блока

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,14 +5,17 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
-  return arr.filter( (num) => num >= a && num <= b )
-
+  // условие позволяет выбрать алгоритм фильтрации, в зависимости какой аргумент больше,
+  // далее фильтруем массив по одному из условий (например num >= a && num <= b)
+  // и возвращаем новый массив отфильтрованных чисел
+  return a < b ? arr.filter( num => num >= a && num <= b ) : arr.filter( num => num >= b && num <= a );
 }
+
 /*
 
 let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 7);
+let filtered = filterRange(arr, 8, 8);
 
 console.log( filtered ); // [3,1] (совпадающие значения)
 console.log( arr ); // [5,3,8,1] (без изменений)

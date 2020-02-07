f91f51f54aebac469f3c254abd4022fa3d130d79
Oleg Chabak <chabak.o@brinex.ru>
2020-02-07 10:40:03 +0300

    Решил 4 задачу 3 блока

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -3,4 +3,18 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
+  const names = [];
+  users.map( (user) => {
+    names.push(user.name)
+  });
+  return names;
 }
+/*
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+let users = [ vasya, petya, masha ];
+
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
+console.log(names);
+*/

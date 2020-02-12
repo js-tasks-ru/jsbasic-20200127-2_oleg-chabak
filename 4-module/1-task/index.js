f9cf244e3689ffb6b1936c10eb3896d33e43f759
Oleg Chabak <chabak.o@brinex.ru>
2020-02-12 17:12:20 +0300

    Выполнил все задачи 4 блока

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,11 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let friendsList = document.createElement('ul');
+  for (friend of friends) {
+    friendsList.appendChild(document.createElement('li'));
+    friendsList.lastChild.innerText = friend.firstName;
+  }
+  console.log(friendsList);
+  return friendsList;
 }
